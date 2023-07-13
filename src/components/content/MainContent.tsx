b792e5c1e59ca1a2cc38d1ac88e21d7e65d14a04
ShueiYang <yangshuei83@gmail.com>
2023-07-13 02:31:28 +0200

    Fix warning message issue with computation

--- src/components/content/MainContent.tsx
+++ src/components/content/MainContent.tsx
@@ -14,11 +14,13 @@ export interface cartProps {
 
 const MainContent = (props: RambuteauRawData) => {
 
+  // use decorator /* @once */ to tell the SolidJS compiler to explicitly not to make this a reactive expression
+  // evaluated once and only once when the DOM nodes are created from the JSX. So we can remove the warning...
 
   return (
     <div class="w-full flex flex-col">
       <div class="sticky top-[72px] z-10">
-        <MenuNavbar  layoutDatas={props.datas.layoutNavigation}/>
+        <MenuNavbar layoutDatas={/*@once*/props.datas.layoutNavigation}/>
       </div>
       <div class="container flex flex-col md:flex-row bg-slate-100 pt-8">
         <MenuLists datas={props.datas} /> 
