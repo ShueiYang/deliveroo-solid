cb3e7692b10c81488999d546cc88e6484aad6a23
ShueiYang <yangshuei83@gmail.com>
2023-08-11 13:19:13 +0200

    update the condition rendering on the createRessource

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { type Component, Show } from 'solid-js';
+import { type Component, Switch, Match } from 'solid-js';
 import MainContent from "./components/content/MainContent";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Loader from './components/Loader';
 import { pageData } from './store';
+import Error from './components/Error';
 
 
 const App: Component = () => {
   
   return (
-    <Show 
-      when={!pageData.loading && pageData()}
-      fallback={<Loader />}
-    >
-      { pageData => 
-        <>
-          <Header datas={pageData()}/>
-          <MainContent datas={pageData()}/>
-          <Footer />
-        </> 
-      }
-    </Show>
+    <Switch>
+      <Match when={pageData.loading}>
+        <Loader />
+      </Match>
+      <Match when={pageData.error}>
+        <Error />
+      </Match>
+      <Match when={pageData.state === "ready"}>  
+        <Header datas={pageData()!}/>
+        <MainContent datas={pageData()!}/>
+        <Footer />
+      </Match>
+    </Switch>
   );
 };
 
