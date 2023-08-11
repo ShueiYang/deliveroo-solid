68016ecfdf66d719884ddc702cf0f13fc969b1a9
ShueiYang <yangshuei83@gmail.com>
2023-08-11 13:18:12 +0200

    add error page

---
+++ src/components/Error.tsx
@@ -0,0 +1,14 @@
+
+
+
+const Error = () => {
+  return (
+    <div class="container flex flex-col items-center justify-center h-screen">
+      <h1 class="text-3xl">
+        An Error occured when fetching data from Deliveroo API
+      </h1>
+    </div>
+  );
+};
+
+export default Error;
